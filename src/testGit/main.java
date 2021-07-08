ca3ae35ec7faca55252c4d6996d0755fe4a472fc
DESKTOP-ALVIN\Z <alvinyap0717@gmail.com>
2021-07-09 01:38:08 +0800

    new things from feature2

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -5,6 +5,11 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("This is a git test project");
 		System.out.println("This is new line feature1");
+		System.out.println(showItem());
+	}
+	
+	public static String showItem() {
+		return "new things from feature2";
 	}
 
 }
