08f030dbdafbb4151930fc3e893fa14098a6c3b7
DESKTOP-ALVIN\Z <alvinyap0717@gmail.com>
2021-07-09 01:34:55 +0800

    new items from master

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("This is a git test project");
 		System.out.println("This is new line feature1");
+		System.out.println("This is new items from master");
 	}
 
 }
