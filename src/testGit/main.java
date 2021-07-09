2b644c951c6a8a27fa4c75bb3e180d8a68569431
alvinyap <iforte.intellege.xhungin@daimler.com>
2021-07-09 10:23:47 +0800

    new master

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -3,17 +3,6 @@ package testGit;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("This is a git test project");
-		System.out.println("This is new line feature1");
-		System.out.println("This is new items from master");
-		System.out.println(showItem());
 		System.out.println("new items from master");
-	}
-	
-	public static String showItem() {
-		return "new things from feature2";
-	}
-	public static String showItem2() {
-		return "show item";
 	}
 }
