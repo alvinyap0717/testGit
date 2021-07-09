3b46f9ae0392d874d724328c60ef46450c1a6498
alvinyap <iforte.intellege.xhungin@daimler.com>
2021-07-09 11:17:09 +0800

    merge master

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -5,5 +5,6 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("new items from master");
 		System.out.println("new items from master2");
+		System.out.println("new items from master3");
 	}
 }
