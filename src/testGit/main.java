2b07dd10d69d3f568d004b9c31e165a8949dfacc
alvinyap0717 <87143355+alvinyap0717@users.noreply.github.com>
2021-07-09 11:38:50 +0800

    Update main.java

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -4,8 +4,5 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("new items from master");
-		System.out.println("new items from master2");
-		System.out.println("new items from master3");
-		System.out.println("new items from master4");
 	}
 }
