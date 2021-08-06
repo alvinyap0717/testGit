577fd9fc0dba9eb4a255fc962317b46999e8570b
alvinyap0717 <87143355+alvinyap0717@users.noreply.github.com>
2021-08-06 15:46:08 +0800

    Update main.java

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -4,7 +4,5 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("initial item");
-		System.out.println("master item 1");
-		System.out.println("feature item 1");
 	}
 }
