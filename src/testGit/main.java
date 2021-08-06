08ac8f361d5f9640946349b24ac86da928d7d15a
alvinyap0717 <87143355+alvinyap0717@users.noreply.github.com>
2021-08-06 16:05:11 +0800

    feature test 1

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -4,7 +4,6 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("initial item");
-		System.out.println("master item 1");
-		System.out.println("feature item 1");
+		System.out.println("feature test item 1");
 	}
 }
