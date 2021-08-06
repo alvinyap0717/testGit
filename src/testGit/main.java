29f3d5505e3da5c0d1f2015c8b5feb0ca7952ccf
alvinyap <iforte.intellege.xhungin@daimler.com>
2021-08-06 16:02:47 +0800

    feature 1 commit

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -5,5 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("initial item");
 		System.out.println("feature test item 1");
+		System.out.println("feature item 1");
+		System.out.println("feature item 2");
+		System.out.println("feature item 3");
 	}
 }
